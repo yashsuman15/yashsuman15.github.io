48d1957eecc218cd854e57dc47b0310c42c77ef1
Yash Raj Suman <yashsuman15@gmail.com>
2026-02-27 12:11:57 +0530

    Set base path for Vite configuration

--- vite.config.ts
+++ vite.config.ts
@@ -16,7 +16,7 @@ export default defineConfig({
       '@': path.resolve(__dirname, './src'),
     },
   },
-
+  base: '/',
   // File types to support raw imports. Never add .css, .tsx, or .ts files to this.
   assetsInclude: ['**/*.svg', '**/*.csv'],
 })
